d0fd2ad1237a080652c74ab15ce32b7885cee199
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-02-07 22:09:47 +1100

    changed port on  server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 80;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
